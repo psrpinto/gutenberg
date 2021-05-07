ce8d70ffdf1ed3e46b582e67c74874c17b0a59e9
Dave Smith <getdavemail@gmail.com>
2021-05-07 16:45:02 +0100

    Move convertMenuItemsToBlocks to own dedicated file

---
+++ packages/block-library/src/navigation/convert-menu-items-to-blocks.js
@@ -0,0 +1,21 @@
+/**
+ * Internal dependencies
+ */
+import createDataTree from './create-data-tree';
+import mapMenuItemsToBlocks from './map-menu-items-to-blocks';
+
+/**
+ * Convert a flat menu item structure to a nested blocks structure.
+ *
+ * @param {Object[]} menuItems An array of menu items.
+ *
+ * @return {WPBlock[]} An array of blocks.
+ */
+export default function convertMenuItemsToBlocks( menuItems ) {
+	if ( ! menuItems ) {
+		return null;
+	}
+
+	const menuTree = createDataTree( menuItems );
+	return mapMenuItemsToBlocks( menuTree );
+}
